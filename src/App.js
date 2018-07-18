5ba8608e3c7aa39a5de4b1f12d9fc1188cf71ba5
kigi0429 <leo@Messis-Mac.local>
2018-07-18 15:59:23 -0700

    add json view

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
+import ReactJson from 'react-json-view';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      error: false,
+      message: "",
+      posts: []
+    };
+  }
+  async componentDidMount() {
+    try {
+      this.setState({message: "Please wait until fetch posts data..."})
+      let result = await fetch('https://thewirecutter.com/wp-json/wp/v2/posts');
+
+
+      let jsonData = await result.json();
+      this.setState({posts: jsonData, loading: false, message: ""});
+    } catch (e) {
+      this.setState({message: "Data fetch failed", error: true})
+      console.log(e)
+    }
+  }
+
   render() {
+    const { message, posts } = this.state
+    let maybeMessageOrPostview = null
+
+    if (message) {
+      maybeMessageOrPostview = <p className="App-intro"> {message} </p>
+    } else {
+      maybeMessageOrPostview = <ReactJson src={posts} />
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to TestApp</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {maybeMessageOrPostview}
       </div>
     );
   }
 }
+
 
 export default App;
